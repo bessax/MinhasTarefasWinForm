efb0a139e32e94f38bf6163221a31134930ef67d
bessax <andrebessax@gmail.com>
2023-10-24 21:29:19 -0300

    #ajustes

--- MinhasTarefas/Tarefas.cs
+++ MinhasTarefas/Tarefas.cs
@@ -5,6 +5,6 @@ public class Tarefas
     public string? Nome { get; set; }
     public string? Descricao { get; set; }
     public bool Status { get; set; }
-    public DateTime DataInicio { get; set; }
-    public DateTime DataFim { get; set; }
+    public DateTime? DataInicio { get; set; }
+    public DateTime? DataFim { get; set; }
 }
